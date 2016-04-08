15a7a1ae3e6e6bba5d58a82be860b64f60af602c
Sullivan ATATRI <satatri@MacBook-Pro-de-Sullivan.local>
2016-04-08 11:07:59 +0200

    [add] admin notification if Timber is not installed

--- configuration/theme_setup/autoload.php
+++ configuration/theme_setup/autoload.php
@@ -2,6 +2,18 @@
 /**
  ** Load after setup theme
  **/
+
+// Send admin notices if Timber isn't installed
+if( !is_plugin_active('timber-library/timber.php') ) {
+    add_action( 'admin_notices', function(){
+        $class = 'notice notice-error';
+        $message = __( "L'extension Timber doit être installé pour pouvoir utiliser ce thème.", 'dummy-wordpress' );
+
+        printf( '<div class="%1$s"><p>%2$s</p></div>', $class, $message );
+    });
+}
+
+// Load textdomain
 add_action('after_setup_theme', function(){
     require_once('textdomain.php');
 });
