f77d16b4d639bde18af9864e8674fe24f90b9ba8
Plou <cl.plou@gmail.com>
2017-01-31 14:34:19 +0100

    init: Put back basic configuration  Add menus, images size & breadcrumb

--- functions.php
+++ functions.php
@@ -4,10 +4,10 @@ require_once(__DIR__ . '/vendor/autoload.php');
 $timber = new \Timber\Timber();
 
 // If the Timber plugin isn't activated, print a notice in the admin.
-if ( ! class_exists( 'Timber' ) ) {
-    add_action( 'admin_notices', function() {
-            echo '<div class="error"><p>Timber not activated. Make sure you activate the plugin in <a href="' . esc_url( admin_url( 'plugins.php#timber' ) ) . '">' . esc_url( admin_url( 'plugins.php' ) ) . '</a></p></div>';
-        } );
+if (!class_exists('Timber')) {
+    add_action('admin_notices', function() {
+            echo '<div class="error"><p>Timber not activated. Make sure you activate the plugin in <a href="' . esc_url(admin_url('plugins.php#timber')) . '">' . esc_url(admin_url('plugins.php')) . '</a></p></div>';
+        });
     return;
 }
 
@@ -17,15 +17,20 @@ class StarterSite extends TimberSite {
 
     // This function applies some fundamental WordPress setup, as well as our functions to include custom post types and taxonomies.
     function __construct() {
-        add_theme_support( 'post-formats' );
-        add_theme_support( 'post-thumbnails' );
-        add_theme_support( 'menus' );
-        add_filter( 'timber_context', array( $this, 'add_to_context' ) );
-        add_filter( 'get_twig', array( $this, 'add_to_twig' ) );
-        add_action( 'init', array( $this, 'register_post_types' ) );
-        add_action( 'init', array( $this, 'register_taxonomies' ) );
-        add_action( 'init', array( $this, 'register_menus' ) );
-        add_action( 'init', array( $this, 'register_widgets' ) );
+        add_theme_support('post-thumbnails');
+        add_theme_support('menus');
+
+        add_filter('timber_context', array($this, 'add_to_context'));
+        add_filter('get_twig', array($this, 'add_to_twig'));
+        add_filter('wp_mail_from', 'noreply@domain.com');
+        add_filter('wp_mail_from_name', 'Administrateur');
+
+        add_action('init', array($this, 'register_post_types'));
+        add_action('init', array($this, 'register_taxonomies'));
+        add_action('init', array($this, 'register_menus'));
+        add_action('init', array($this, 'register_widgets'));
+        add_action('init', array($this, 'add_image_size'));
+
         parent::__construct();
     }
 
@@ -52,15 +57,29 @@ class StarterSite extends TimberSite {
         require('lib/widgets.php');
     }
 
+    function add_image_size(){
+        add_image_size('post_home_thumbnail', 400, 300, true );
+    }
+
 
     // Access data site-wide.
 
     // This function adds data to the global context of your site. In less-jargon-y terms, any values in this function are available on any view of your website. Anything that occurs on every page should be added here.
 
-    function add_to_context( $context ) {
+    function add_to_context($context) {
 
         // Our menu occurs on every page, so we add it to the global context.
-        $context['menu'] = new TimberMenu();
+        $context['menu'] = array(
+            'main' => new TimberMenu('main'),
+            'footer' => new TimberMenu('footer'),
+        );
+
+        $context['post_type'] = get_post_type();
+
+        // breadCrump
+        if(function_exists('yoast_breadcrumb')){
+            $context['breadcrumb'] = yoast_breadcrumb('<p>','</p>', false );
+        }
 
         // This 'site' context below allows you to access main site information like the site title or description.
         $context['site'] = $this;
@@ -69,9 +88,9 @@ class StarterSite extends TimberSite {
 
     // Here you can add your own fuctions to Twig. Don't worry about this section if you don't come across a need for it.
     // See more here: http://twig.sensiolabs.org/doc/advanced.html
-    function add_to_twig( $twig ) {
-        $twig->addExtension( new Twig_Extension_StringLoader() );
-        $twig->addFilter( 'myfoo', new Twig_Filter_Function( 'myfoo' ) );
+    function add_to_twig($twig) {
+        $twig->addExtension(new Twig_Extension_StringLoader());
+        $twig->addFilter('myfoo', new Twig_Filter_Function('myfoo'));
         return $twig;
     }
 
@@ -95,8 +114,8 @@ function my_scripts() {
 
     // Enqueue our stylesheet and JS file with a jQuery dependency.
     // Note that we aren't using WordPress' default style.css, and instead enqueueing the file of compiled Sass.
-    wp_enqueue_style( 'my-styles', get_template_directory_uri() . '/assets/css/main.css', 1.0);
-    wp_enqueue_script( 'my-js', get_template_directory_uri() . '/assets/js/main.js', array('jquery'), '1.0.0', true );
+    wp_enqueue_style('my-styles', get_template_directory_uri() . '/assets/css/main.css', 1.0);
+    wp_enqueue_script('my-js', get_template_directory_uri() . '/assets/js/main.js', array('jquery'), '1.0.0', true );
 }
 
-add_action( 'wp_enqueue_scripts', 'my_scripts' );
+add_action('wp_enqueue_scripts', 'my_scripts');
--- lib/menus.php
+++ lib/menus.php
@@ -7,3 +7,5 @@
  */
 
 // Register your menus here.
+register_nav_menu('main', __('Menu principal', 'inouit'));
+register_nav_menu('footer', __('Menu du pied de page', 'inouit'));
